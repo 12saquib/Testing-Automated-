584373fab55cece2cad10f8a8f2b5fa8333e3e18
12saquib <56392783+12saquib@users.noreply.github.com>
2021-09-12 19:23:23 +0530

    Update SeleniumProject.java

--- SeleniumProject.java
+++ SeleniumProject.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumProject {
 
 	public static void main(String[] args) throws InterruptedException{
-		  System.setProperty("webdriver.chrome.driver", "C:\\Users\\Saquib Jawed\\Downloads\\chromedriver_win32\\chromedriver.exe");
+          System.setProperty("webdriver.chrome.driver", "C:\\Users\\Saquib Jawed\\Downloads\\chromedriver_win32\\chromedriver.exe");
           ChromeDriver driver =new ChromeDriver();
           String url="https://www.thesparksfoundationsingapore.org/";
           driver.manage().window().maximize();
