1c710646184ceaf3956033e3813cbb83d0250ed7
12saquib <56392783+12saquib@users.noreply.github.com>
2021-09-14 15:44:21 +0530

    Update SeleniumProject.java

--- SeleniumProject.java
+++ SeleniumProject.java
@@ -11,6 +11,12 @@ public class SeleniumProject {
           driver.manage().window().maximize();
           driver.get(url);
           Thread.sleep(3000);
+	  driver.findElement(By.linkText("Glimpses for Kids")).click();
+          Thread.sleep(3000);
+          driver.findElement(By.linkText("Student Scholarship Program")).click();
+          Thread.sleep(3000);
+          driver.findElement(By.linkText("Career Choices")).click();
+          Thread.sleep(3000);
           driver.findElement(By.linkText("Contact Us")).click();
           Thread.sleep(3000);
           driver.findElement(By.linkText("GRIP (Internship)")).click();
@@ -30,6 +36,13 @@ public class SeleniumProject {
           driver.findElement(By.linkText("Management Volunteer")).click();
           Thread.sleep(3000);
           driver.findElement(By.linkText("Jobs at Tech in Asia Portal")).click();
+	  Thread.sleep(10000);
+          driver.findElement(By.linkText("Jobs at Angel.co Portal")).click();
+          Thread.sleep(10000);
+          driver.findElement(By.linkText("The Sparks Foundation (Global)")).click();
+          Thread.sleep(10000);
+          driver.findElement(By.linkText("Internships at Internshala")).click();
+          Thread.sleep(10000);
           driver.close();
           driver.quit();
 	}
